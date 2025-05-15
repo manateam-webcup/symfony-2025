138aede67bd4b448a2b62ffbb75bb44ce62c2ab3
Arivalen <ben.motean@gmail.com>
2025-05-15 18:06:05 +0400

    create login form, redirects on success/failure and logout functionality

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -5,14 +5,34 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
-final class LoginController extends AbstractController
+class LoginController extends AbstractController
 {
     #[Route('/login', name: 'app_login')]
-    public function index(): Response
+    public function index(AuthenticationUtils $authenticationUtils): Response
     {
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        $lastUsername = $authenticationUtils->getLastUsername();
+
         return $this->render('login/index.html.twig', [
-            'controller_name' => 'LoginController',
+            'last_username' => $lastUsername,
+            'error' => $error,
         ]);
+    }
+
+    #[Route('/success', name: 'login_success')]
+    public function checkloginStatus(AuthenticationUtils $authenticationUtils): Response
+    {
+        $lastUsername = $authenticationUtils->getLastUsername();
+        if ($lastUsername) {
+            $this->addFlash('success', 'Welcome back, ' . $lastUsername . '!');
+            return $this->render('login/success.html.twig');
+        } else {
+            $this->addFlash('error', 'You are not logged in.');
+            return $this->render('base.html.twig');
+        }
+//        return $this->render('login/success.html.twig');
     }
 }
